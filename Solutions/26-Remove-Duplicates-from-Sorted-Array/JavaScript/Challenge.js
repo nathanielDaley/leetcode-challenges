169f5022b4e1ae82d8cffdc551323623c2f02389
Nathaniel Daley <ndaley64@gmail.com>
2025-03-10 13:54:55 -0500

    add problem 26

---
+++ Solutions/26-Remove-Duplicates-from-Sorted-Array/JavaScript/Challenge.js
@@ -0,0 +1,46 @@
+// ------------------------------
+// Solutions
+// ------------------------------
+// Solution 1
+// ------------------------------
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  uniqueArray = [];
+  uniqueCount = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!uniqueArray.includes(nums[i])) {
+      uniqueArray.push(nums[i]);
+      nums[uniqueCount++] = nums[i];
+    }
+  }
+
+  return uniqueCount;
+};
+
+// ------------------------------
+// Solution 2
+// ------------------------------
+var removeDuplicates = function (nums) {
+  uniqueCount = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[uniqueCount]) {
+      nums[++uniqueCount] = nums[i];
+    }
+  }
+
+  return ++uniqueCount;
+};
+
+// ------------------------------------------
+// Tests
+// ------------------------------------------
+let nums = [1, 1, 2];
+console.log(removeDuplicates(nums), nums);
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+console.log(removeDuplicates(nums), nums);
